777393d4f91c2532861824666b02e97c750d6eb5
Christopher Scully <scullycphone@gmail.com>
2020-06-13 00:18:40 -0500

    update

---
+++ Project/KioskUI 2.py
@@ -0,0 +1,26 @@
+import kivy
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+kivy.require('1.11.1')
+
+Builder.load_file('screen.kv')
+
+# Declare the screens
+class MenuScreen(Screen):
+    pass
+
+class SettingsScreen(Screen):
+    def changeSettings(self):
+        print('Settings Changed')
+    pass
+
+sm = ScreenManager()
+sm.add_widget(MenuScreen(name='menu'))
+sm.add_widget(SettingsScreen(name='settings'))
+sm.current = 'menu'
+
+class Menu(App):
+    def build(self):
+        return sm
