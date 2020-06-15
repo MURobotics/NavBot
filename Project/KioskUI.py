c5a9787b73fa05dc0c7076c897ce6d5d97da1aa0
Christopher Scully <scullycphone@gmail.com>
2020-06-15 13:53:57 -0500

    left align, text input update

--- Project/KioskUI.py
+++ Project/KioskUI.py
@@ -6,15 +6,21 @@ from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import ListProperty, ObjectProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.vkeyboard import VKeyboard
+from kivy.config import Config
 
 kivy.require('1.11.1')
 
 Builder.load_file('Template.kv')
 Builder.load_file('Map.kv')
 Builder.load_file('Settings.kv')
+Config.set('kivy', 'keyboard_mode', 'systemandmulti')
 
 class LocationOption(Button):
     pass
+
+class Test(VKeyboard):
+    player = VKeyboard()
 
 # Declare the screens
 class MenuScreen(Screen):
@@ -24,31 +30,31 @@ class MenuScreen(Screen):
         self.name = 'home'
         super(Screen, self).__init__(**kwargs)
         self.renderRooms()
-
     pass
 
     def navigationTest(self):
         print('Screen Call')
-        #This function won't execute much, but will call the connection.py classes to get relevant info and then probably a navigation.py for running navigation to the destination
+        # This function won't execute much, but will call the connection.py classes to get relevant info and then probably a navigation.py for running navigation to the destination
 
     def renderRooms(self):
         self.renderLoc.clear_widgets()
         for i in range(1, 20):
             temp = LocationOption()
-            self.renderLoc.add_widget(LocationOption(text='TEST: '+str(i)))
-
-        return
+            self.renderLoc.add_widget(LocationOption(text=self.search.text + str(i)))
 
 class SettingsScreen(Screen):
 
     def changeSettings(self):
         print('Settings Changed')
+
     pass
+
 
 sm = ScreenManager()
 sm.add_widget(MenuScreen(name='menu'))
 sm.add_widget(SettingsScreen(name='settings'))
 sm.current = 'menu'
+
 
 class Menu(App):
     def build(self):
