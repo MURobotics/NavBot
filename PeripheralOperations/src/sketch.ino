e9ec49dcf358c97e3e5b96d34d525f7bb0573cc5
Christopher Scully <scullycphone@gmail.com>
2020-06-25 12:41:00 -0500

    Update sketch.ino

--- PeripheralOperations/src/sketch.ino
+++ PeripheralOperations/src/sketch.ino
@@ -1,15 +1,33 @@
+/*
+ * rosserial Publisher Example
+ * Prints "hello world!"
+ */
+
+// Use the following line if you have a Leonardo or MKR1000
+//#define USE_USBCON
+
 #include <ros.h>
 #include <std_msgs/String.h>
 
-int ledPin = LED_BUILTIN;
+nh.getHardware()->setBaud(9600)
 
-void setup(){
-    pinMode(ledPin, OUTPUT);
+ros::NodeHandle nh;
+
+std_msgs::String str_msg;
+ros::Publisher chatter("chatter", &str_msg);
+
+char hello[13] = "hello world!";
+
+void setup()
+{
+  nh.initNode();
+  nh.advertise(chatter);
 }
 
-void loop(){
-    digitalWrite(ledPin, HIGH);
-    delay(3000);
-    digitalWrite(ledPin, LOW);
-    delay(3000);
+void loop()
+{
+  str_msg.data = hello;
+  chatter.publish( &str_msg );
+  nh.spinOnce();
+  delay(1000);
 }
