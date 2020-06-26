82bf1b1b7951c98c3054196687e94cfb12bfa121
Christopher Scully <scullycphone@gmail.com>
2020-06-26 16:01:18 -0500

    Update sketch.ino

--- PeripheralOperations/src/sketch.ino
+++ PeripheralOperations/src/sketch.ino
@@ -4,25 +4,43 @@
  */
 
 #include <ros.h>
+#include <std_msgs/String.h>
 #include <std_msgs/Empty.h>
 
 ros::NodeHandle nh;
 
-void messageCb( const std_msgs::Empty& toggle_msg){
-  digitalWrite(13, HIGH-digitalRead(13));   // blink the led
-}
+std_msgs::String str_msg;
+ros::Publisher chatter("output", &str_msg);
+ros::Subscriber<std_msgs::Empty> sub("toggle_chat", &messageCb );
 
-ros::Subscriber<std_msgs::Empty> sub("toggle_led", &messageCb );
+int i = 0;
+char hello[13] = "hello world!";
+
+void messageCb( const std_msgs::Empty& toggle_msg){
+  if (i == 0){
+    i = 1;
+  }
+  else if (i == 1){
+    i = 0;
+  }
+  else {
+    i = 0;
+  }
+}
 
 void setup()
 {
-  pinMode(13, OUTPUT);
   nh.initNode();
   nh.subscribe(sub);
+  nh.advertise(chatter);
 }
 
 void loop()
 {
+  if(i == 1){
+    str_msg.data = hello;
+    chatter.publish( &str_msg );
+  }
   nh.spinOnce();
-  delay(1);
+  delay(1000);
 }
