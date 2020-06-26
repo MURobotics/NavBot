e62eb798f032790c54a91b217d57189921d2526c
Christopher Scully <scullycphone@gmail.com>
2020-06-26 15:44:50 -0500

    Update sketch.ino

--- PeripheralOperations/src/sketch.ino
+++ PeripheralOperations/src/sketch.ino
@@ -1,36 +1,28 @@
 /*
- * rosserial::std_msgs::Float64 Test
- * Receives a Float64 input, subtracts 1.0, and publishes it
+ * rosserial Subscriber Example
+ * Blinks an LED on callback
  */
 
 #include <ros.h>
-#include <std_msgs/Float64.h>
-
-float x;
+#include <std_msgs/Empty.h>
 
 ros::NodeHandle nh;
 
-ROS_CALLBACK(messageCb, std_msgs::Float64, msg){
-  x = msg.data - 1.0;
+void messageCb( const std_msgs::Empty& toggle_msg){
   digitalWrite(13, HIGH-digitalRead(13));   // blink the led
 }
 
-std_msgs::Float64 test;
-ros::Subscriber s("your_topic", &msg, &messageCb);
-ros::Publisher p("my_topic", &test);
+ros::Subscriber<std_msgs::Empty> sub("toggle_led", &messageCb );
 
 void setup()
 {
   pinMode(13, OUTPUT);
   nh.initNode();
-  nh.advertise(p);
-  nh.subscribe(s);
+  nh.subscribe(sub);
 }
 
 void loop()
 {
-  test.data = x;
-  p.publish( &test );
   nh.spinOnce();
-  delay(10);
+  delay(1);
 }
