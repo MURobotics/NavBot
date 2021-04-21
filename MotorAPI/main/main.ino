18230da75474a27a71475de3b59a08f423e16b0f
Cameron Ellis <cameronellis46@gmail.com>
2021-04-21 16:54:37 -0500

    Update Motor Pins for Motor Controller
    Updated motor pins for each motor to work with motor controller. Each motor needs two pins for each motor (one for high, the other for low)

--- MotorAPI/main/main.ino
+++ MotorAPI/main/main.ino
@@ -1,9 +1,13 @@
 //Motor Pin Setup
 void setup() {
-  const byte frLM = 0; //front left motor
-  const byte frRM = 1; //font right motor
-  const byte bckLM = 2; //back left motor
-  const byte bckRM = 3; //back right motor
+  const byte frLM1 = 0; //front left motorpin 1
+  const byte frLM2 = 1; //front left motorpin 2
+  const byte frRM1 = 2; //font right motorpin 1
+  const byte frRM2 = 3; //font right motorpin 2
+  const byte bckLM1 = 4; //back left motorpin 1
+  const byte bckLM2 = 5; //back left motorpin 2
+  const byte bckRM1 = 6; //back right motorpin 1
+  const byte bckRM2 = 7; //back right motorpin 2
 }
 
 void loop() {
